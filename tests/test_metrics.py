ba2f1a78a772d6d4e3f1c8cae5fab6373c70fc1a
Dung LT. <thync@outlook.com>
2024-09-17 13:50:49 +0700

    Add: tests

---
+++ tests/test_metrics.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import vstatistic as vs
+
+data = pd.read_csv('returns.csv', index_col='Date', parse_dates=True)
+prices = data.Close
+print(prices)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+metrics = vs.metrics.metrics(prices, mode="full", display=False)
+print(metrics)
